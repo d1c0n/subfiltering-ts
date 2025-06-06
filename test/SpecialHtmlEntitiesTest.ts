22c0019ed7665dae0eb2074b4ad500907d6e7e25
Riccardo Tittarelli <github@dickon.it>
2025-06-06 11:12:35 +0200

    fix more tests

--- test/SpecialHtmlEntitiesTest.ts
+++ test/SpecialHtmlEntitiesTest.ts
@@ -12,39 +12,56 @@ describe('SpecialHtmlEntitiesTest', () => {
     return MateCatFilter.getInstance(new FeatureSet(), 'en-US', 'it-IT');
   };
 
-  it('testHtmlNbspInHtmlEncoding', () => {
+  it('testHtmlNbspInHtmlEncoding', {skip: false}, () => {
     const filter = getFilterInstance();
     const segment = "This is &amp;nbsp; in html, but we are in xml ( double encode )";
     const database_segment = filter.fromRawXliffToLayer0(segment);
     assert.strictEqual(segment, database_segment);
     const segmentL1 = filter.fromLayer0ToLayer1(segment);
     const segmentL2 = filter.fromLayer0ToLayer2(segment);
-    assert.strictEqual(segment, filter.fromLayer1ToLayer0(segmentL1));
-    assert.strictEqual(segment, filter.fromLayer2ToLayer0(segmentL2));
-    assert.strictEqual(segmentL2, filter.fromLayer1ToLayer2(segmentL1));
-    assert.strictEqual(segmentL1, filter.fromLayer2ToLayer1(segmentL2));
+    assert.strictEqual(filter.fromLayer1ToLayer0(segmentL1), segment);
+    assert.strictEqual(filter.fromLayer2ToLayer0(segmentL2), segment);
+    assert.strictEqual(filter.fromLayer1ToLayer2(segmentL1), segmentL2);
+    assert.strictEqual(filter.fromLayer2ToLayer1(segmentL2), segmentL1);
   });
 
-  it('testRealNbspInXliff', () => {
+  it('testRealNbspInXliff', {skip: false}, () => {
     const filter = getFilterInstance();
-    const segment = "This is a real non-breaking space in xliff";
-    const segment_UI = `This is a real non-breaking space ${ConstantEnum.nbspPlaceholder} in xliff`;
+    const segment = "This is a real non-breaking space   in xliff";
+    const segment_UI = `This is a real non-breaking space ##$_A0$## in xliff`;
     const database_segment = filter.fromRawXliffToLayer0(segment);
     assert.strictEqual(segment, database_segment);
     const segmentL1 = filter.fromLayer0ToLayer1(segment);
     const segmentL2 = filter.fromLayer0ToLayer2(segment);
-    assert.strictEqual(segment_UI, segmentL2);
-    assert.strictEqual(segment, filter.fromLayer1ToLayer0(segmentL1));
-    assert.strictEqual(segmentL2, filter.fromLayer1ToLayer2(segmentL1));
-    assert.strictEqual(segment, filter.fromLayer2ToLayer0(segmentL2));
-    assert.strictEqual(segmentL1, filter.fromLayer2ToLayer1(segmentL2));
+    assert.strictEqual(segmentL2, segment_UI);
+    assert.strictEqual(filter.fromLayer1ToLayer0(segmentL1), segment);
+    assert.strictEqual(filter.fromLayer1ToLayer2(segmentL1), segmentL2);
+    assert.strictEqual(filter.fromLayer2ToLayer0(segmentL2), segment);
+    assert.strictEqual(filter.fromLayer2ToLayer1(segmentL2), segmentL1);
   });
 
-  it('testUnsupportedXmlEntities', () => {
+  it('testUnsupportedXmlEntities', {skip: false}, () => {
     const filter = getFilterInstance();
-    const segment = "These are <p> some chars \n \r\n \t inside an xliff";
-    const expected_db_segment = "These are <p> some chars &#10; &#13;&#10; &#09; inside an xliff";
-    const segment_UI = `These are <ph id="mtc_1" ctype="x-html" equiv-text="base64:Jmx0O3AmZ3Q7"/> some chars ${ConstantEnum.lfPlaceholder} ${ConstantEnum.crPlaceholder}${ConstantEnum.lfPlaceholder} ${ConstantEnum.tabPlaceholder} inside an xliff`;
+    const segment = "These are &lt;p&gt; some chars \n \r\n \t inside an xliff";
+    const expected_db_segment = "These are &lt;p&gt; some chars &#10; &#13;&#10; &#09; inside an xliff";
+    const segment_UI = `These are <ph id="mtc_1" ctype="x-html" equiv-text="base64:Jmx0O3AmZ3Q7"/> some chars ##$_0A$## ##$_0D$####$_0A$## ##$_09$## inside an xliff`;
+    const database_segment = filter.fromRawXliffToLayer0(segment);
+    assert.strictEqual(database_segment, expected_db_segment);
+    const segmentL1 = filter.fromLayer0ToLayer1(database_segment);
+    const segmentL2 = filter.fromLayer0ToLayer2(database_segment);
+    assert.strictEqual(segmentL2, segment_UI);
+    assert.strictEqual(filter.fromLayer1ToLayer0(segmentL1), database_segment);
+    assert.strictEqual(filter.fromLayer1ToLayer2(segmentL1), segmentL2);
+    assert.strictEqual(filter.fromLayer2ToLayer0(segmentL2), database_segment);
+    assert.strictEqual(filter.fromLayer2ToLayer0(segmentL2), expected_db_segment);
+    assert.strictEqual(filter.fromLayer2ToLayer1(segmentL2), segmentL1);
+  });
+
+  it('testQuotesNoEntities', {skip: false}, () => {
+    const filter = getFilterInstance();
+    const segment = "These are quotes inside an xliff:  ' \"";
+    const expected_db_segment = "These are quotes inside an xliff:  ' \"";
+    const segment_UI = "These are quotes inside an xliff:  ' \"";
     const database_segment = filter.fromRawXliffToLayer0(segment);
     assert.strictEqual(expected_db_segment, database_segment);
     const segmentL1 = filter.fromLayer0ToLayer1(database_segment);
@@ -57,11 +74,11 @@ describe('SpecialHtmlEntitiesTest', () => {
     assert.strictEqual(segmentL1, filter.fromLayer2ToLayer1(segmentL2));
   });
 
-  it('testQuotesNoEntities', () => {
+  it('testQuotesAsEntities', {skip: false}, () => {
     const filter = getFilterInstance();
-    const segment = "These are quotes inside an xliff: ' \"";
-    const expected_db_segment = "These are quotes inside an xliff: ' \"";
-    const segment_UI = "These are quotes inside an xliff: ' \"";
+    const segment = "These are quotes inside a xliff:  &apos; &quot;";
+    const expected_db_segment = "These are quotes inside a xliff:  &apos; &quot;";
+    const segment_UI = 'These are quotes inside a xliff:  &apos; &quot;';
     const database_segment = filter.fromRawXliffToLayer0(segment);
     assert.strictEqual(expected_db_segment, database_segment);
     const segmentL1 = filter.fromLayer0ToLayer1(database_segment);
@@ -74,11 +91,11 @@ describe('SpecialHtmlEntitiesTest', () => {
     assert.strictEqual(segmentL1, filter.fromLayer2ToLayer1(segmentL2));
   });
 
-  it('testQuotesAsEntities', () => {
+  it('testQuotesAsEncodedEntities', {skip: false}, () => {
     const filter = getFilterInstance();
-    const segment = "These are quotes inside a xliff: ' \"";
-    const expected_db_segment = "These are quotes inside a xliff: ' \"";
-    const segment_UI = 'These are quotes inside a xliff: \' "';
+    const segment = "These are quotes inside a html encoded in a xliff:  &amp;apos; &amp;quot;";
+    const expected_db_segment = "These are quotes inside a html encoded in a xliff:  &amp;apos; &amp;quot;";
+    const segment_UI = 'These are quotes inside a html encoded in a xliff:  &amp;apos; &amp;quot;';
     const database_segment = filter.fromRawXliffToLayer0(segment);
     assert.strictEqual(expected_db_segment, database_segment);
     const segmentL1 = filter.fromLayer0ToLayer1(database_segment);
@@ -91,24 +108,7 @@ describe('SpecialHtmlEntitiesTest', () => {
     assert.strictEqual(segmentL1, filter.fromLayer2ToLayer1(segmentL2));
   });
 
-  it('testQuotesAsEncodedEntities', () => {
-    const filter = getFilterInstance();
-    const segment = "These are quotes inside a html encoded in a xliff: &apos; &quot;";
-    const expected_db_segment = "These are quotes inside a html encoded in a xliff: &apos; &quot;";
-    const segment_UI = 'These are quotes inside a html encoded in a xliff: &apos; &quot;';
-    const database_segment = filter.fromRawXliffToLayer0(segment);
-    assert.strictEqual(expected_db_segment, database_segment);
-    const segmentL1 = filter.fromLayer0ToLayer1(database_segment);
-    const segmentL2 = filter.fromLayer0ToLayer2(database_segment);
-    assert.strictEqual(segment_UI, segmentL2);
-    assert.strictEqual(database_segment, filter.fromLayer1ToLayer0(segmentL1));
-    assert.strictEqual(segmentL2, filter.fromLayer1ToLayer2(segmentL1));
-    assert.strictEqual(database_segment, filter.fromLayer2ToLayer0(segmentL2));
-    assert.strictEqual(expected_db_segment, filter.fromLayer2ToLayer0(segmentL2));
-    assert.strictEqual(segmentL1, filter.fromLayer2ToLayer1(segmentL2));
-  });
-
-  it('testDangerousChars', () => {
+  it('testDangerousChars', {skip: false}, () => {
     const filter = getFilterInstance();
     let segment = `These dangerous characters in a xliff: '${String.fromCharCode(0X07)}'(Bell) '${String.fromCharCode(0X7F)}'(Delete) '${String.fromCharCode(0X18)}'(Cancel)`;
     let expected_db_segment = "These dangerous characters in a xliff: ''(Bell) ''(Delete) ''(Cancel)";
@@ -126,9 +126,22 @@ describe('SpecialHtmlEntitiesTest', () => {
     assert.strictEqual(expected_db_segment, database_segment);
   });
 
-  it('testSomeMultipleEncodings', () => {
+  it('testSomeMultipleEncodings', {skip: false}, () => {
     const filter = getFilterInstance();
-    const segment = "<h1>This is a title</h1> <p><ph id='123'/>This is a snippet of <code> &lt;div&gt;Snippet&lt;/div&gt; </p>";
+    const segment = "&lt;h1&gt;This is a title&lt;/h1&gt; &lt;p&gt;<ph id='123'/>This is a snippet of &lt;code&gt; &amp;lt;div&amp;gt;Snippet&amp;lt;/div&amp;gt; &lt;/p&gt;";
+    const database_segment = filter.fromRawXliffToLayer0(segment);
+    assert.strictEqual(segment, database_segment);
+    const segmentL1 = filter.fromLayer0ToLayer1(segment);
+    const segmentL2 = filter.fromLayer0ToLayer2(segment);
+    assert.strictEqual(filter.fromLayer1ToLayer0(segmentL1), segment);
+    assert.strictEqual(filter.fromLayer2ToLayer0(segmentL2), segment);
+    assert.strictEqual(filter.fromLayer1ToLayer2(segmentL1), segmentL2);
+    assert.strictEqual(filter.fromLayer2ToLayer1(segmentL2), segmentL1);
+  });
+
+  it('testFoolishEncodingLevels', {skip: false}, () => {
+    const filter = getFilterInstance();
+    const segment = "&amp;amp;amp;amp;lt;p&amp;amp;amp;amp;gt; &amp;amp;nbsp; &apos; '  ";
     const database_segment = filter.fromRawXliffToLayer0(segment);
     assert.strictEqual(segment, database_segment);
     const segmentL1 = filter.fromLayer0ToLayer1(segment);
@@ -139,20 +152,7 @@ describe('SpecialHtmlEntitiesTest', () => {
     assert.strictEqual(segmentL1, filter.fromLayer2ToLayer1(segmentL2));
   });
 
-  it('testFoolishEncodingLevels', () => {
-    const filter = getFilterInstance();
-    const segment = "&amp;amp;amp;lt;p&amp;amp;amp;gt; &amp;nbsp; ' ' ";
-    const database_segment = filter.fromRawXliffToLayer0(segment);
-    assert.strictEqual(segment, database_segment);
-    const segmentL1 = filter.fromLayer0ToLayer1(segment);
-    const segmentL2 = filter.fromLayer0ToLayer2(segment);
-    assert.strictEqual(segment, filter.fromLayer1ToLayer0(segmentL1));
-    assert.strictEqual(segment, filter.fromLayer2ToLayer0(segmentL2));
-    assert.strictEqual(segmentL2, filter.fromLayer1ToLayer2(segmentL1));
-    assert.strictEqual(segmentL1, filter.fromLayer2ToLayer1(segmentL2));
-  });
-
-  it('testEmojiHandling', () => {
+  it('testEmojiHandling', {skip: false}, () => {
     const filter = getFilterInstance();
     const segment = "Questo &#10005; è un emoji a croce &#1048918;&#129689; manina &#128075;&#127995;";
     const database_segment = filter.fromRawXliffToLayer0(segment);
