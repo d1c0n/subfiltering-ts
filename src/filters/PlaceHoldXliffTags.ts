c389e3ea84c005e3b8301dc7cb59ca996bf1c5d0
Riccardo Tittarelli <github@dickon.it>
2025-06-06 11:11:30 +0200

    fix regex

--- src/filters/PlaceHoldXliffTags.ts
+++ src/filters/PlaceHoldXliffTags.ts
@@ -5,32 +5,32 @@ export class PlaceHoldXliffTags extends AbstractHandler {
   public transform(segment: string): string {
     // input : <g id="43">bang & < 3 olufsen </g>; <x id="33"/>
     // remove not existent </x> tags
-    segment = segment.replace(/(<\/x>)/si, '');
+    segment = segment.replace(/(<\/x>)/gsi, '');
 
-    segment = segment.replace( /<(g\s*id=[\"\']+.*?[\"\']+\s*[^<>]*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(g\s*xid=[\"\']+.*?[\"\']+\s*[^<>]*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(g\s*id=[\"\']+.*?[\"\']+\s*[^<>]*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(g\s*xid=[\"\']+.*?[\"\']+\s*[^<>]*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
 
-    segment = segment.replace( /<(\/g)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(\/g)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
 
-    segment = segment.replace( /<(x .*?\/?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(bx[ ]{0,}\/?\/bx .*?\/?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(ex[ ]{0,}\/?   \/ex .*?\/?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(bpt\s*.*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(\/bpt)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(ept\s*.*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(\/ept)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(ph .*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(\/ph)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(ec .*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(\/ec)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(sc .*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(\/sc)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(pc .*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(\/pc)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(it .*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(\/it)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(mrk\s*.*?)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
-    segment = segment.replace( /<(\/mrk)>/si, `${ConstantEnum.LTPLACEHOLDER}\$1{ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(x .*?\/?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(bx[ ]{0,}\/?\/bx .*?\/?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(ex[ ]{0,}\/?   \/ex .*?\/?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(bpt\s*.*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(\/bpt)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(ept\s*.*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(\/ept)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(ph .*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(\/ph)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(ec .*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(\/ec)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(sc .*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(\/sc)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(pc .*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(\/pc)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(it .*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(\/it)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(mrk\s*.*?)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
+    segment = segment.replace( /<(\/mrk)>/gsi, `${ConstantEnum.LTPLACEHOLDER}\$1${ConstantEnum.GTPLACEHOLDER}`);
 
 
     return segment.replace(new RegExp(`${ConstantEnum.LTPLACEHOLDER}(.*?)${ConstantEnum.GTPLACEHOLDER}`, 'gu'), (match, p1) => {
